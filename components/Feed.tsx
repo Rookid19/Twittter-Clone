73d7bff09a677bd3722cf527564d8b5d739ae3d5
roodev <randyodoom19@gmail.com>
2022-05-31 18:38:59 +0000

    changes in twailwind.config.js

---
+++ components/Feed.tsx
@@ -0,0 +1,13 @@
+import { RefreshIcon } from '@heroicons/react/outline'
+import React from 'react'
+
+function Feed() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <RefreshIcon className='h-8 w-8 cursor-pointer text-twitter transition-all duration-500 ease-out hover:rotate-180 active:scale-125'/>
+    </div>
+  )
+}
+
+export default Feed
