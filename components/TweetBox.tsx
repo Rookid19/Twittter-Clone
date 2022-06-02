916640a3b87c62542fc5ea560999858d21612c03
roodev <randyodoom19@gmail.com>
2022-06-02 10:37:17 +0000

    tweetBox

---
+++ components/TweetBox.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TweetBox() {
+  return (
+    <div>
+      <img src="https://links.papareact.com/gll" alt="" />
+    </div>
+  )
+}
+
+export default TweetBox
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,9 @@ import Feed from '../components/Feed'
 import Sidebar from '../components/Sidebar'
 import Widgets from '../components/Widgets'
 
-
 const Home: NextPage = () => {
   return (
-    <div >
+    <div className="lg:max-w-6xl mx-auto max-h-screen overflow-hidden">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
