5ddde991ba44e5ece911b299e67034f113a8c85f
roodev <randyodoom19@gmail.com>
2022-06-02 09:14:38 +0000

    twitter timeline embed

--- components/Widgets.tsx
+++ components/Widgets.tsx
@@ -1,8 +1,24 @@
+import { SearchIcon } from '@heroicons/react/outline'
 import React from 'react'
+import {TwitterTimelineEmbed} from 'react-twitter-embed'
 
 function Widgets() {
   return (
-    <div>Widgets</div>
+    <div className="mt-2 px-2">
+      <div className="flex items-center space-x-2 bg-gray-100 p-3 rounded-full">
+        <SearchIcon className="h-5 w-5 text-gray-400" />
+        <input
+          type="text"
+          placeholder="Search Twitter"
+          className="flex-1 outline-none bg-transparent"
+        />
+      </div>
+      <TwitterTimelineEmbed
+        sourceType="profile"
+        screenName="saurabhnemade"
+        options={{ height: 400 }}
+      />
+    </div>
   )
 }
 
