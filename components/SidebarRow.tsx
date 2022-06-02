3178e3eefdde042a1d10e5c0b8d6750e7554200f
roodev <randyodoom19@gmail.com>
2022-06-02 10:17:10 +0000

    responsiveness

--- components/SidebarRow.tsx
+++ components/SidebarRow.tsx
@@ -10,7 +10,9 @@ function SidebarRow({ Icon, title }: Props) {
   return (
     <div className="flex max-w-fit items-center space-x-2 px-4 py-3 cursor-pointer rounded-full transition-all duration-200 hover:bg-gray-100 group">
       <Icon className="h-6 w-6" />
-      <p className='group-hover:text-twitter'>{title}</p>
+      <p className="hidden md:inline-flex group-hover:text-twitter text-base font-light lg:text-xl">
+        {title}
+      </p>
     </div>
   )
 }
--- components/Widgets.tsx
+++ components/Widgets.tsx
@@ -1,10 +1,11 @@
 import { SearchIcon } from '@heroicons/react/outline'
 import React from 'react'
-import {TwitterTimelineEmbed} from 'react-twitter-embed'
+import { TwitterTimelineEmbed } from 'react-twitter-embed'
 
 function Widgets() {
   return (
-    <div className="mt-2 px-2 col-span-2">
+    //widget only shows on the large screen but hides on the small screen with the help of the hidden feature in the css
+    <div className="mt-2 px-2 col-span-2 hidden lg:inline">
       <div className="flex items-center space-x-2 bg-gray-100 p-3 rounded-full">
         <SearchIcon className="h-5 w-5 text-gray-400" />
         <input
